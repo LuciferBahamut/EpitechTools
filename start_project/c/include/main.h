3fcb3ebbb39a9779096972076209319d6e4589d2
Quentin TREHEUX <quentin.treheux@epitech.eu>
2021-03-14 17:57:47 +0100

    [ADD] include for c project

---
+++ start_project/c/include/main.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** EpitechTools
+** File description:
+** main
+*/
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* INCLUDE */
+#include <stdbool.h>
+
+/* STATIC CONST */
+static const int SUCCESS = 0;
+static const int FAIL = 1;
+static const int ERROR = 84;
+
+#endif /* !MAIN_H_ */
